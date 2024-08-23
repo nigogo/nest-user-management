f9075b9cbdd4781febc1554d2aac9fd8cba8472c
Nico Harather <nicoharather@gmail.com>
2024-08-23 14:20:17 +0200

    Implement /register route

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,14 +1,15 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 import { RegisterUserDto } from './dto/register-user.dto';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
 @ApiTags('auth')
 export class AuthController {
-	constructor() {}
+	constructor(private readonly authService: AuthService) {}
 
 	@Post('register')
 	async register(@Body() registerUserDto: RegisterUserDto) {
-		return JSON.stringify(registerUserDto);
+		return this.authService.register(registerUserDto);
 	}
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,29 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
+import { RegisterUserDto } from './dto/register-user.dto';
+import { UserDto } from './dto/user.dto';
+import { PrismaService } from '../prisma/prisma.service';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+	private readonly logger = new Logger(AuthService.name);
+
+	constructor(private readonly prisma: PrismaService) {}
+
+	async register({ username, password }: RegisterUserDto): Promise<UserDto> {
+		try {
+			const user = await this.prisma.user.create({
+				data: {
+					username,
+					password,
+				},
+			});
+
+			this.logger.log(`User ${user.username} created`);
+
+			return user;
+		} catch (e) {
+			this.logger.error(e.message);
+			throw e;
+		}
+	}
+}
